f40cf8bf2b21386902d0bee4966caf00baf932eb
nguyenhuydev <nguyenhuyhuy04041998@gmail.com>
2024-11-21 15:08:30 +0700

    [feat]: create groups table

---
+++ database/migrations/2024_11_21_041929_create_groups_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable();
+            $table->string('slug', 255)->nullable();
+            $table->text('content')->nullable();
+            $table->integer('status')->nullable();
+            $table->integer('parent_id')->nullable();
+            $table->text('keywords')->nullable();
+            $table->text('description')->nullable();
+            $table->text('title')->nullable();
+            $table->text('image_share')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
